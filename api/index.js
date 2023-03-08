820e9e20fa8d75aff0754d373ece3ab1f55689b2
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2023-03-08 15:30:08 +0530

    change in backend base url

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiInstance = axios.create({
-  baseURL: "http://10.7.2.154:3001/api/v1",
+  baseURL: "http://192.168.0.100:3001/api/v1",
 });
 
 export default apiInstance;
