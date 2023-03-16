27680d797a631078f438ff41e8b710aeb6aa5089
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2023-03-16 11:45:39 +0530

    chaged backend url

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiInstance = axios.create({
-  baseURL: "http://192.168.0.100:3001/api/v1",
+  baseURL: "http://192.168.0.106:3001/api/v1",
 });
 
 export default apiInstance;
